6f1b376873b384772cdc52a1fceb5e7fa4689403
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-08-02 14:36:35 -0300

    add cors and body-parser

--- app.js
+++ app.js
@@ -2,11 +2,33 @@
 
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
 const routeProducts = require('./routes/products');
 const routeRequests = require('./routes/requests');
-const morgan = require('morgan');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false})); // only data simple
+app.use(bodyParser.json()); // only json data 
+
+//cors:
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        );
+
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods',
+        'PUT, POST, PATCH, DELETE, GET');
+
+        return res.status(200).send({});
+    }
+
+    next();
+});
 
 app.use('/products', routeProducts);
 app.use('/requests', routeRequests);
