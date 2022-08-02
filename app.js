43790cd250b80bc5d1b5491213011cbbb15bfd56
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-08-02 11:37:54 -0300

    Tratamento de rotas não encontradas

--- app.js
+++ app.js
@@ -4,8 +4,28 @@ const express = require('express');
 const app = express();
 const routeProducts = require('./routes/products');
 const routeRequests = require('./routes/requests');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 app.use('/products', routeProducts);
 app.use('/requests', routeRequests);
+
+
+// this stretch is a treatment for when you can't find a route:
+app.use((req, res, next) => {
+    const error =  new Error('Not finded');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) =>{
+    res.status(error.status || 500);
+    return res.send({
+       error: {
+        message: error.message
+       } 
+    });
+});
 
 module.exports = app;
